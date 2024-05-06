5e9c79daf5ee5c260d1448f3ba526f4dec6f03a5
ChristopherLerum <146862377+ChristopherLerum@users.noreply.github.com>
2024-05-06 13:07:26 -0700

    working speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,49 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  
+  const mySpeech = window.speechSynthesis;
+  console.log(speechSynthesis.getVoices());
+  const tts = new SpeechSynthesisUtterance();
+  const mylist = document.querySelector("select");
+
+  document.querySelector("textarea").addEventListener('input', (event) => {
+    tts.text = document.querySelector("textarea").value;
+  });
+
+  document.querySelector("button").addEventListener('click', (event) => {
+    document.querySelector("img").setAttribute("src", "assets/images/smiling-open.png");
+    let voices = speechSynthesis.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      console.log('1');
+      if (voices[i].name === mylist.selectedOptions[0].getAttribute("data-name")) {
+        console.log('2');
+        tts.voice = voices[i];
+        break;
+      }
+    }
+    mySpeech.speak(tts);
+    tts.onend = function (event) {
+      document.querySelector("img").setAttribute("src", "assets/images/smiling.png");
+    };
+  });
+
+  document.querySelector("select").addEventListener('click', morev);
+  document.querySelector("select").addEventListener('input', vchoice);
+}
+
+function morev() {
+  let voices = speechSynthesis.getVoices();
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.querySelector("select").appendChild(option);
+  }
+}
+
+function vchoice() {
+  let voices = speechSynthesis.getVoices();
 }
